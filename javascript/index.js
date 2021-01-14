fc529dff2b19bf022403f010550416a6513644a7
dadddda <daduka9k@gmail.com>
2021-01-14 05:29:58 +0400

    Reimplemented initBtns() to support one-page webapp

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 // variables and constants
 const leftPanel = document.getElementById("leftPanel");
 const btns = leftPanel.getElementsByClassName("leftPanelBtn");
-const btnDelayMs = 400;
+const btnDelayMs = 200;
 
 /**
  * Redirects page to login.html if the user isn't logged in.
@@ -16,7 +16,8 @@ firebase.auth().onAuthStateChanged(function(user) {
  * Event listener for header left button.
  */
 document.getElementById("headerLeftBtn").addEventListener("click", function() {
-    document.getElementById("rightPanel").classList.add("hidden");
+    let rightPanel = document.getElementById("rightPanel");
+    rightPanel.classList.add("hidden");
     setTimeout(function() {
         window.location.assign("index.html");
     }, btnDelayMs);
@@ -51,25 +52,52 @@ function initBtns() {
     for (let i = 0; i < btns.length; i++) {
         btns[i].addEventListener("click", function(event) {
             let curr = leftPanel.getElementsByClassName("active");
-            if (curr.length != 0) curr[0].classList.remove("active");
+            if (curr.length != 0) {
+                if (curr[0].id == event.target.id) return;
+                curr[0].classList.remove("active");
+            }
             
             event.target.classList.add("active");
             
-            document.getElementById("rightPanel").classList.add("hidden");
+            let rightPanel = document.getElementById("rightPanel");
+            rightPanel.classList.add("hidden");
             setTimeout(function() {
+                let rightPanel = document.getElementById("rightPanel");
+                rightPanel.removeChild(rightPanel.firstElementChild);
+                rightPanel.classList.remove("hidden");
+                let newElem = document.createElement("div");
+
                 switch (event.target.id) {
                     case "newsFeedBtn":
-                        window.location.assign("news-feed.html");
+                        newElem.className = "newsFeed";
+                        rightPanel.appendChild(newElem);
+
+                        new newsFeed(newElem);
                         break;
                     case "clinicsBtn":
+                        newElem.className = "clinics";
+                        rightPanel.appendChild(newElem);
+
                         break;
                     case "supportedTestsBtn":
+                        newElem.className = "supportedTests";
+                        rightPanel.appendChild(newElem);
+
                         break;
-                    case "testingResults":
+                    case "testingResultsBtn":
+                        newElem.className = "testingResults";
+                        rightPanel.appendChild(newElem);
+
                         break;
-                    case "contactUs":
+                    case "contactUsBtn":
+                        newElem.className = "contactUs";
+                        rightPanel.appendChild(newElem);
+
                         break;
-                    case "aboutUs":
+                    case "aboutUsBtn":
+                        newElem.className = "aboutUs";
+                        rightPanel.appendChild(newElem);
+
                         break;
                 }
             }, btnDelayMs);
