07fff33591a7954730780747ab14268422a14624
dadddda <daduka9k@gmail.com>
2021-01-27 20:20:29 +0400

    Initial implementation of news 'More' button

--- javascript/news-feed.js
+++ javascript/news-feed.js
@@ -13,25 +13,10 @@ class NewsFeed {
         this.newsRef = firebase.firestore().collection("news");
         this.latestInit = false;
         this.latestDoc = null;
-
+    
+        this.clickHandlerRef = this.clickHandler.bind(this);
         this.scrollHandlerRef = this.scrollHandler.bind(this);
         this.windowResizeHandlerRef = this.windowResizeHandler.bind(this);
-    }
-
-    /**
-     * Initializes event listeners.
-     */
-    initListeners() {
-        this.newsFeedElem.addEventListener("scroll", this.scrollHandlerRef);
-        window.addEventListener("resize", this.windowResizeHandlerRef);
-    }
-
-    /**
-     * Deinitializes event listeners.
-     */
-    deinitListeners() {
-        this.newsFeedElem.removeEventListener("scroll", this.scrollHandlerRef);
-        window.removeEventListener("resize", this.windowResizeHandlerRef);
     }
 
     /**
@@ -65,6 +50,7 @@ class NewsFeed {
         }
     
         data.docs.forEach(doc => {
+            let id = doc.id;
             let title = doc.data().title;
             let timestamp = doc.data().date.toMillis();
             let content = doc.data().content;
@@ -72,7 +58,7 @@ class NewsFeed {
             content = content.replaceAll("\\n", "<br><br>");
             
             let formattedDate = this.createDate(timestamp);
-            this.buildAndAppendNews(title, formattedDate, content);
+            this.buildAndAppendNews(id, title, formattedDate, content);
         });
             
         this.latestDoc = data.docs[data.docs.length - 1];
@@ -112,15 +98,29 @@ class NewsFeed {
 
     /**
      * Builds new template of HTML element of news feed 
-     * according to given data and calls this.appendNews()
+     * according to given data and calls 'this.appendNews()'
      * function.
-     * @param {string} title 
-     * @param {string} date 
-     * @param {string} content 
+     * @param {String} id 
+     * @param {String} title 
+     * @param {String} date 
+     * @param {String} content 
      */
-    buildAndAppendNews(title, date, content) {
+    buildAndAppendNews(id, title, date, content) {
+        let wordCount = this.countWords(content);
+
+        let moreBtn = "";
+        if (wordCount > 128) {
+            content = this.substrWords(content, 32);
+            content += "...";
+            moreBtn = `
+                <div class="moreBtnContainer">
+                    <button class="moreBtn" id="moreBtn">More</button>
+                </div>
+            `;
+        }
+
         let html = `
-            <div class="news">
+            <div class="news" id="${id}">
                 <div class="newsHeader">
                     <div class="newsTitle">
                         ${title}
@@ -133,10 +133,125 @@ class NewsFeed {
                 <div class="newsContent">
                     ${content}
                 </div>
+                ${moreBtn}
             </div>
         `;
-        
+
         appendHtml(html, this.newsFeedElem);
+    }
+
+    /**
+     * 
+     * @param {String} id
+     */
+    async drawOpenedNews(id) {
+        var docRef = this.newsRef.doc(id);
+
+        await docRef.get().then(doc => {
+            if (doc.exists) {
+                let title = doc.data().title;
+                let content = doc.data().content;
+                content = content.replaceAll("\\n", "<br><br>");
+                let html = `
+                    <div class="openedNewsBackground" style="top: ${this.newsFeedElem.scrollTop}px;"></div>
+                    <div class="openedNews" style="top: ${this.newsFeedElem.scrollTop + 50}px;">
+                        <div class="openedNewsTitle">
+                            <text class="titleText">${title}</text>
+                            <img class="actionBtn" id="closeBtn" src="../svgs/close.svg">
+                        </div>
+                        <div class="openedNewsContent">
+                            ${content}
+                        </div>
+                    </div>
+                `;
+
+                appendHtml(html, this.newsFeedElem);
+            } else {
+                console.log("No such document.");
+            }
+        }).catch(error => {
+            console.log("Error: ", error);
+        });
+    }
+
+    /**
+     * Returns rough word count in given string.
+     * @param {String} string 
+     */
+    countWords(string) {
+        let wordCount = 0;
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] == ' ') wordCount++;
+        }
+
+        return wordCount;
+    }
+
+    /**
+     * Returns 'wordCount' number of words as string from given 'string'.
+     * @param {String} string
+     * @param {Number} wordCount 
+     */
+    substrWords(string, wordCountLim) {
+        let substr = "";
+        let wordCount = 0;
+        let word = "";
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] == ' ') {
+                substr += word;
+                wordCount++;
+                word = "";
+
+                if (wordCount < wordCountLim) substr += ' ';
+                else i = string.length;
+            } else {
+                word += string[i];
+            }
+        }
+
+        return substr;
+    }
+
+    /**
+     * Initializes event listeners.
+     */
+    initListeners() {
+        this.newsFeedElem.addEventListener("click", this.clickHandlerRef);
+        this.newsFeedElem.addEventListener("scroll", this.scrollHandlerRef);
+        window.addEventListener("resize", this.windowResizeHandlerRef);
+    }
+
+    /**
+     * Deinitializes event listeners.
+     */
+    deinitListeners() {
+        this.newsFeedElem.removeEventListener("click", this.clickHandlerRef);
+        this.newsFeedElem.removeEventListener("scroll", this.scrollHandlerRef);
+        window.removeEventListener("resize", this.windowResizeHandlerRef);
+    }
+
+    /**
+     * Handles click events.
+     * @param {Event} event 
+     */
+    clickHandler(event) {
+        if (event.target.id == "moreBtn") {
+            this.newsFeedElem.style.overflow = "hidden";
+            let parentElem = event.target.parentElement.parentElement;
+            this.drawOpenedNews(parentElem.id);
+        } else if (event.target.id == "closeBtn") {
+            this.newsFeedElem.style.overflow = "overlay";
+
+            let openedNewsBackgroundElem = document.getElementsByClassName("openedNewsBackground")[0];
+            let openedNewsElem = document.getElementsByClassName("openedNews")[0];
+            openedNewsBackgroundElem.style.opacity = 0;
+            openedNewsElem.style.opacity = 0;
+
+            setTimeout(() => {
+                this.newsFeedElem.removeChild(openedNewsBackgroundElem);
+                this.newsFeedElem.removeChild(openedNewsElem);
+            }, 200);
+        }
     }
 
     /**
